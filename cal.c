72635014bfbeda38e9bc785479a7e10b4955b3bf
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-02-27 17:12:15 -0500

    feat: add substring helper function

--- cal.c
+++ cal.c
@@ -22,3 +22,9 @@ typedef struct {
 
 #define STR8_LIT(s) (string8){(u8 *)(s), sizeof((s)) - 1}
 #define STR8_FMT(s) (int)(s8).size, (s8).str
+
+string8 str8_substr(string8 str, u64 start, u64 end) {
+  end = MIN(end, str.size);
+  start = MIN(start, end);
+  return (string8){str.str + start, end - start};
+}
